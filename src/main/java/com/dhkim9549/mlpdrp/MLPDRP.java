00394dfb72e86056034b32d7c7a4e16d721f3433
dhkim9549 <dhkim95@hotmail.com>
2017-10-29 11:37:15 +0900

    aaa

--- src/main/java/com/dhkim9549/mlpdrp/MLPDRP.java
+++ src/main/java/com/dhkim9549/mlpdrp/MLPDRP.java
@@ -42,7 +42,7 @@ public class MLPDRP {
     static long nEvalSamples = 10000;
 
     // Number of input variables to the neural network
-    static int numOfInputs = 19 - 7;
+    static int numOfInputs = 19 - 4;
 
     static LineNumberReader in = null;
     static String trainingDataInputFileName = "/down/collect_data/collect_data_20130101.txt";
@@ -77,9 +77,10 @@ public class MLPDRP {
 
             if(i % 10000 == 0) {
                 System.out.println("i = " + i);
-                evaluateModel(model);
+                // evaluateModel(model);
             }
-            if(i % 150000 == 0) {
+
+            if(i % 50000 == 0) {
                 MLPDRPEval.evaluateModelBatch(model);
             }
 
@@ -239,10 +240,10 @@ public class MLPDRP {
         featureData[9] = rescaleYn(exempt_yn);
         featureData[10] = rescaleYn(sptrepay_yn);
         featureData[11] = rescaleYn(psvact_yn);
-        /*
         featureData[12] = rescaleNum(rdbtr_1_cnt); // new input
         featureData[13] = rescaleNum(rdbtr_2_cnt); // new input
         featureData[14] = rescaleAmt(age, 0, 100); // new input
+        /*
         featureData[15] = rescaleAmt(dischrg_occr_amt); // new input
         featureData[16] = rescaleYn(prscp_cmplt_yn); // new input
         featureData[17] = rescaleYn(ibon_amtz_yn); // new input
@@ -285,10 +286,10 @@ public class MLPDRP {
             featureData[9] = 0.0;
             featureData[10] = 0.0;
             featureData[11] = 0.0;
-            /*
             featureData[12] = 0.0;; // new input
             featureData[13] = 0.0; // new input
             featureData[14] = rescaleAmt(40, 0, 100); // new input
+            /*
             featureData[15] = rescaleAmt(20000000); // new input
             featureData[16] = rescaleYn("N"); // new input
             featureData[17] = 0.0; // new input
